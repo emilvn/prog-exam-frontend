e7d9795c74b6fc37f3af97cc9f0ccfa16c21ff5d
emilvn <emilvnielsen@hotmail.com>
2024-06-19 20:54:20 +0200

    added example hook + tested connection to backend

--- src/containers/Home.tsx
+++ src/containers/Home.tsx
@@ -1,10 +1,17 @@
 import PageLayout from "../components/PageLayout.tsx";
+import useExamples from "../hooks/useExamples.ts";
 
 function Home() {
+    const { examples } = useExamples();
+
     return (
         <PageLayout>
             <h1>Home</h1>
             <p>Welcome to the Home page!</p>
+            {examples.length > 0 &&
+                examples.map((example) => (
+                    <p key={example.id}>{example.name}</p>
+                ))}
         </PageLayout>
     );
 }
