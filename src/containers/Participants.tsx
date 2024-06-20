0c17f43b699104e0e5fca3c8472fc1d83d3c1420
emilvn <emilvnielsen@hotmail.com>
2024-06-21 01:39:35 +0200

    made filter for participants

--- src/containers/Participants.tsx
+++ src/containers/Participants.tsx
@@ -482,6 +482,7 @@ interface ParticipantsProps {
 function Participants({ search }: ParticipantsProps) {
   const { participants, create, update, remove, addDisciplines, removeDisciplines, isLoading } =
     useParticipants();
+  const { disciplines } = useDisciplines();
   const [filteredParticipants, setFilteredParticipants] = useState<ParticipantWithDisciplines[]>(
     []
   );
@@ -492,6 +493,12 @@ function Participants({ search }: ParticipantsProps) {
   const [showResultsModal, setShowResultsModal] = useState(false);
   const [sortColumn, setSortColumn] = useState<string>("name");
   const [sortOrder, setSortOrder] = useState<"asc" | "desc">("asc");
+  const [selectedFilterOptions, setSelectedFilterOptions] = useState<
+    { label: string; value: string | number; type: string }[]
+  >([]);
+  const [filterOptions, setFilterOptions] = useState<
+    { label: string; value: string | number; type: string }[]
+  >([]);
 
   useEffect(() => {
     if (search && search !== "") {
@@ -508,6 +515,69 @@ function Participants({ search }: ParticipantsProps) {
   useEffect(() => {
     setFilteredParticipants(sortParticipants(sortColumn, sortOrder, filteredParticipants));
   }, [sortColumn, sortOrder]);
+
+  useEffect(() => {
+    setFilterOptions([
+      {
+        label: "Børn",
+        value: "Børn",
+        type: "ageGroup"
+      },
+      {
+        label: "Unge",
+        value: "Unge",
+        type: "ageGroup"
+      },
+      {
+        label: "Junior",
+        value: "Junior",
+        type: "ageGroup"
+      },
+      {
+        label: "Voksne",
+        value: "Voksne",
+        type: "ageGroup"
+      },
+      {
+        label: "Senior",
+        value: "Senior",
+        type: "ageGroup"
+      },
+      {
+        label: "Mænd",
+        value: "Mænd",
+        type: "gender"
+      },
+      {
+        label: "Kvinder",
+        value: "Kvinder",
+        type: "gender"
+      },
+      ...disciplines.map((d) => ({ label: d.name, value: d.id ?? -1, type: "discipline" }))
+    ]);
+  }, [disciplines]);
+
+  useEffect(() => {
+    if (selectedFilterOptions.length > 0) {
+      let fp = participants;
+      selectedFilterOptions.forEach((option) => {
+        fp = fp.filter((participant) => {
+          if (option.value === "Mænd") {
+            return participant.isMale;
+          } else if (option.value === "Kvinder") {
+            return !participant.isMale;
+          } else if (typeof option.value === "number") {
+            return participant.disciplines.some((d) => d.id === option.value);
+          } else {
+            return getAgeGroup(participant) === option.value;
+          }
+        });
+      });
+      setFilteredParticipants(fp);
+    } else {
+      setFilteredParticipants(participants);
+    }
+  }, [selectedFilterOptions]);
 
   return (
     <>
@@ -532,6 +602,59 @@ function Participants({ search }: ParticipantsProps) {
             >
               Opret deltager
             </button>
+          </div>
+          <div className={"w-96 flex items-end gap-2 py-2"}>
+            <label className="flex flex-col ml-4">
+              <span className={"italic text-sm"}>Filtrér</span>
+              <select
+                onChange={(e) => {
+                  const option = filterOptions.find((option) => option.value == e.target.value);
+                  if (!option) return;
+                  setSelectedFilterOptions((prev) => {
+                    if (prev.some((o) => o.type === option.type)) {
+                      return [...prev.filter((o) => o.type !== option.type), option];
+                    }
+                    return [...prev, option];
+                  });
+                }}
+                value={selectedFilterOptions[0]?.value ?? ""}
+                className="border p-1 rounded"
+              >
+                <option value={""}>Alle</option>
+                {filterOptions.map((option) => (
+                  <option
+                    key={option.value}
+                    value={option.value}
+                  >
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </label>
+            <div>
+              {selectedFilterOptions.length > 0 && (
+                <div className={"flex gap-2"}>
+                  {selectedFilterOptions.map((option) => (
+                    <div
+                      key={option.value}
+                      className={
+                        "bg-sky-500 text-white p-1 rounded-lg cursor-pointer hover:bg-sky-300"
+                      }
+                    >
+                      {option.label}
+                      <MdClose
+                        onClick={() => {
+                          setSelectedFilterOptions((prev) =>
+                            prev.filter((o) => o.value !== option.value)
+                          );
+                        }}
+                        className={"inline-block"}
+                      />
+                    </div>
+                  ))}
+                </div>
+              )}
+            </div>
           </div>
           <table className="w-full">
             <thead className="bg-sky-300 text-white text-left">
